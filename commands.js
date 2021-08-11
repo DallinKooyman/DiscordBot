9ca8ddfc853debf9911e1229e071a772db851a7b
Dallin Kooyman <dmkooyman@gmail.com>
2021-08-10 20:30:42 -0600

    Update Command Manager
    This commit adds the ability to read all the JavaScript files in the command folder and put all commands in one Discord Collection. A discord collection extends a JavaScript Map.

--- commands.js
+++ commands.js
@@ -1,5 +1,12 @@
-const replies = require("./commands/eastereggreplies.js")
+const Discord = require('discord.js');
+const allCommands = new Discord.Collection(); //Extends JS Map
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+    const command = require(`./Commands/${file}`);
+    allCommands.set(file, command);
+}
 
 module.exports = async function (msg) {
-  replies(msg);
+  allCommands.get("eastereggreplies.js")(msg);
 }
