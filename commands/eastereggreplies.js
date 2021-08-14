bd07c143ab536da8b67fc474558f027e9cc345c8
Dallin Kooyman <dmkooyman@gmail.com>
2021-08-13 19:49:48 -0600

    Slash Command
    This codes adds the slash command functionality. /ping can be used to have the bot reply with pong

--- commands/eastereggreplies.js
+++ commands/eastereggreplies.js
@@ -1,15 +1,29 @@
-module.exports = async function (msg) {
-  if (msg.content.toLowerCase() === "hello there") {
-    msg.channel.send('General Kenobi!\nYou are a bold one!');
-  }
-  else if (msg.content === "ping") {
-    msg.channel.send('pong');
-  }
-  else if (msg.content === "PING") {
-    msg.channel.send('PONG');
-  }
-  else if (msg.content.toLowerCase() === "ping") {
-    msg.channel.send("pOnG")
-  }
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
-}
+// module.exports = {
+//   data: new SlashCommandBuilder()
+// 		.setName('eastereggreplies')
+// 		.setDescription('Replies with Pong!'),
+// 	async execute(interaction) {
+//     if (interaction.content.toLowerCase() === "hello there") {
+//       await interaction.channel.send('General Kenobi!\nYou are a bold one!');
+//     }
+//     else if (interaction.content === "ping") {
+//       await interaction.channel.send('pong');
+//     }
+//     else if (interaction.content === "PING") {
+//       await interaction.channel.send('PONG');
+//     }
+//     else if (interaction.content.toLowerCase() === "ping") {
+//       await interaction.channel.send("pOnG")
+//     }
+// 	},
+// }
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
