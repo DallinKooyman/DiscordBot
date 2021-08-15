06df2e3cfbcde943e587732088d136aecaae2fc5
Dallin Kooyman <dmkooyman@gmail.com>
2021-08-15 14:38:20 -0600

    Add interactionCreate to event handler
    Move interactionCreate to event handler  and out of main.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,55 @@
+const { Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+
+const slashcommands = new Collection() 
+const slashcommandsJSON = [];
+const BOT_ID = process.env.BOT_ID;
+const TEST_GUILD_ID=process.env.BOT_TESTING_GUILD_ID;
+const token = process.env.BOT_TOKEN;
+
+const slashcommandFiles = fs.readdirSync('slashcommands/').filter(file => file.endsWith('.js'))
+for (const file of slashcommandFiles) {
+    const command = require(`../slashcommands/${file}`);
+		// set a new item in the Collection
+		// with the key as the command name and the value as the exported module
+    slashcommands.set(command.data.name, command);
+		slashcommandsJSON.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			//Routes.applicationGuildCommands(BOT_ID, TEST_GUILD_ID), //for testing on one server
+      Routes.applicationCommands(BOT_ID), //for testing on multiple servers
+			{ body: slashcommandsJSON },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+module.exports = {
+	name: 'interactionCreate',
+	async execute(interaction) {
+    if (!interaction.isCommand()) return;
+  
+    const { commandName: slashcommandName } = interaction;
+
+    if (!slashcommands.has(slashcommandName)) return;
+    try {
+      await slashcommands.get(slashcommandName).execute(interaction);
+
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+	},
+};
--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-const { Client, Collection, Intents, Discord } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 require('dotenv').config();
 
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
 const fs = require('fs');
 
 const token = process.env.BOT_TOKEN;
-const BOT_ID = process.env.BOT_ID;
-const TEST_GUILD_ID=process.env.BOT_TESTING_GUILD_ID;
-
-
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
@@ -24,54 +17,7 @@ for (const file of eventFiles) {
 	}
 }
 
-const commandhandler = require("./commands")
-
-
-const slashcommands = new Collection() 
-const slashcommandsJSON = [];
-
-const slashcommandFiles = fs.readdirSync('./slashcommands/').filter(file => file.endsWith('.js'))
-for (const file of slashcommandFiles) {
-    const command = require(`./slashcommands/${file}`);
-		// set a new item in the Collection
-		// with the key as the command name and the value as the exported module
-    slashcommands.set(command.data.name, command);
-		slashcommandsJSON.push(command.data.toJSON());
-}
-
-
-const rest = new REST({ version: '9' }).setToken(token);
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			//Routes.applicationGuildCommands(BOT_ID, TEST_GUILD_ID),
-      Routes.applicationCommands(BOT_ID), //for testing on multiple servers
-			{ body: slashcommandsJSON },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
-
-bot.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-  
-	const { commandName: slashcommandName } = interaction;
-
-	if (!slashcommands.has(slashcommandName)) return;
-	try {
-		await slashcommands.get(slashcommandName).execute(interaction);
-
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
+const commandhandler = require("./commandHandler")
 
 bot.on('messageCreate', commandhandler);
 bot.login(token);
