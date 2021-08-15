09b6af407b60496282daf211bc32100f38348f53
Dallin Kooyman <dmkooyman@gmail.com>
2021-08-15 12:42:47 -0600

    Add Commands
    This adds commands to the bot. Using the format in eastereggreplies.js one can make any command for the use to call. This is done through a command handler.

--- commands.js
+++ commands.js
@@ -1,3 +1,15 @@
-module.exports = async function (msg) {
-  allCommands.get("eastereggreplies.js")(msg);
+const { Collection, Discord } = require('discord.js');
+const fs = require('fs');
+
+const commands = new Collection() //Extends JS Map
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+		// set a new item in the Collection
+		// with the key as the command name and the value as the exported module
+    commands.set(command.name, command);
+}
+module.exports = function (msg) {
+  commands.get("eastereggreplies").execute(msg);
 }
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const { Client, Collection, Intents, Discord } = require('discord.js');
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const Discord = require('discord.js')
+const { Collection, Intents, } = require('discord.js');
+const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 //const bot = new Discord.Client();
 require('dotenv').config();
 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
 
 const token = process.env.BOT_TOKEN;
 const BOT_ID = process.env.BOT_ID;
@@ -14,32 +16,24 @@ bot.login(token);
 const PREFIX = "!GM ";
 const commandhandler = require("./commands")
 
-const commands = new Collection() //Extends JS Map
-const commandsJSON = [];
-const fs = require('fs');
+const slashcommands = new Collection() 
+const slashcommandsJSON = [];
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+const slashcommandFiles = fs.readdirSync('./slashcommands/').filter(file => file.endsWith('.js'))
+for (const file of slashcommandFiles) {
+    const command = require(`./slashcommands/${file}`);
 		// set a new item in the Collection
 		// with the key as the command name and the value as the exported module
-    commands.set(command.data.name, command);
-		commandsJSON.push(command.data.toJSON());
+    slashcommands.set(command.data.name, command);
+		slashcommandsJSON.push(command.data.toJSON());
 }
+
+
 
 bot.once('ready', () => {
   console.log('This bot is online!');
   bot.user.setActivity('Aoe2: The Electric Boogaloo')
 })
-
-// bot.on('message', msg => {
-//   if (msg.author.bot){
-//     return;
-//   }
-//   commandhandler(msg);
-// });
-
-
 
 const rest = new REST({ version: '9' }).setToken(token);
 
@@ -50,7 +44,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 		await rest.put(
 			//Routes.applicationGuildCommands(BOT_ID, TEST_GUILD_ID),
       Routes.applicationCommands(BOT_ID), //for testing on multiple servers
-			{ body: commandsJSON },
+			{ body: slashcommandsJSON },
 		);
 
 		console.log('Successfully reloaded application (/) commands.');
@@ -62,16 +56,16 @@ const rest = new REST({ version: '9' }).setToken(token);
 bot.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const { commandName: slashcommandName } = interaction;
 
-	if (!commands.has(commandName)) return;
+	if (!slashcommands.has(slashcommandName)) return;
 	try {
-		await commands.get(commandName).execute(interaction);
+		await slashcommands.get(slashcommandName).execute(interaction);
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-//TODO:
-// Figure out how to use applications commands (line 50ish), something is weird
+bot.on('messageCreate', commandhandler);
+
